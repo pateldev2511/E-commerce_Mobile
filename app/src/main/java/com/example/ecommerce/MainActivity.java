75d23424f6631276d335fd73a0b13718d1011bb0
Dev Patel <dev@devs-air.myfiosgateway.com>
2023-01-28 11:50:03 -0500

    01/28/2023

---
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button joinNowButton, loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        joinNowButton = (Button) findViewById(R.id.main_join_now_bt);
+        loginButton = (Button) findViewById(R.id.main_login_bt);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, loginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
